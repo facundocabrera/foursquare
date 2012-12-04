e3633a11749264d7aba894bcdf213679ab60f36b
Facundo Cabrera <cabrerafacundo@gmail.com>
2012-12-04 17:50:10 -0300

    Photo API

---
+++ photo.js
@@ -0,0 +1,40 @@
+$(function (){
+
+  var file = null;
+
+  $('[type=file]').change(function(){
+      file = this.files[0];
+  });
+
+  $('button').click(function(){
+    var formData = new FormData($('form')[0]);
+
+    debugger;
+
+    $.ajax({
+        url: 'https://api.foursquare.com/v2/users/self/update?oauth_token=JIZVPW03C2B3HNDJV504JUWZVGWEQVALYNG4DGSXPBH5HH5V&v=20121110',
+        type: 'POST',
+        xhr: function() {  // custom xhr
+            myXhr = $.ajaxSettings.xhr();
+            if(myXhr.upload){ // check if upload property exists
+                myXhr.upload.addEventListener('progress',progressHandlingFunction, false); // for handling the progress of the upload
+            }
+            return myXhr;
+        },
+        // Form data
+        data: formData,
+
+        // Options to tell JQuery not to process data or worry about content-type
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+  });
+
+  function progressHandlingFunction(e){
+    if(e.lengthComputable){
+        $('progress').attr({value:e.loaded,max:e.total});
+    }
+  }
+
+});
